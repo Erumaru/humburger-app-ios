dc340c9db66bc280372a0ba183615ccdbb4df2a5
erumaru <toremuratuly.abzal@gmail.com>
2020-09-05 13:27:33 +0500

    Initial Commit

---
+++ humber-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  humber-app
+//
+//  Created by Abzal Toremuratuly on 9/5/20.
+//  Copyright © 2020 Abzal Toremuratuly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ humber-appTests/humber_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  humber_appTests.swift
+//  humber-appTests
+//
+//  Created by Abzal Toremuratuly on 9/5/20.
+//  Copyright © 2020 Abzal Toremuratuly. All rights reserved.
+//
+
+import XCTest
+@testable import humber_app
+
+class humber_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
